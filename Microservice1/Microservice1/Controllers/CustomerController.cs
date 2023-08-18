343a065e0a3cf410582dde11fcff1b274efe4ec5
Md. Tahsinur Rahman Habib <56672054+Tahsinur77@users.noreply.github.com>
2023-08-19 01:57:46 +0600

    create customer model & controller. In controller calling ISendEndpoint interface and send massage to Customer-Create-Queue

---
+++ Microservice1/Microservice1/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Share;
+
+namespace Microservice1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ISendEndpointProvider endpoint;
+
+        public CustomerController(ISendEndpointProvider _endpoint)
+        {
+            endpoint = _endpoint ?? throw new NullReferenceException(nameof(_endpoint));
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] CustomerModel cmd)
+        {
+            ISendEndpoint sendEndpoint = await endpoint.GetSendEndpoint(new($"queue:{QueueService.CustomerCreateQueue}"));
+            await sendEndpoint.Send(cmd);
+            return Ok("Success");
+        }
+    }
+}
