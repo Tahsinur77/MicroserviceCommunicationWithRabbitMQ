eb5ed116c8dc2fe07982bb8d1319f2a1c7540f67
Md. Tahsinur Rahman Habib <56672054+Tahsinur77@users.noreply.github.com>
2023-08-19 01:16:03 +0600

    create a common folder for entity and set up rabbitMq send-end point for MicroService1

--- Microservice1/Microservice1/Program.cs
+++ Microservice1/Microservice1/Program.cs
@@ -1,3 +1,7 @@
+using MassTransit;
+using Microsoft.Extensions.Options;
+using Share;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//add service to the container using Options Pattern 
+builder.Services.Configure<RabbitMqConfiguration>( options => 
+    builder.Configuration.GetSection("RabbitMq").Bind(options));
+
+var rabbitMqConfig = builder.Services.BuildServiceProvider()
+    .GetRequiredService<IOptions<RabbitMqConfiguration>>().Value;
+
+builder.Services.AddMassTransit(config =>
+    config.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(new Uri(rabbitMqConfig.Url), hst =>
+        {
+            hst.Username(rabbitMqConfig.User);
+            hst.Password(rabbitMqConfig.Password);
+        });
+    })
+);
+
 
 var app = builder.Build();
 
---
+++ Microservice1/Share/RabbitMqConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share
+{
+    public class RabbitMqConfiguration
+    {
+        public string Url { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public int RetryCount { get; set; }
+        public int RetryIntervalSecond { get; set; }
+    }
+}
